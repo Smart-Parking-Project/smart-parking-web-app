eee5b749eda6bbc1efe7f9dc25e5a2eb2a1b0685
sandeeplayam <sandeeplayam@cmail.carleton.ca>
2021-01-14 18:46:16 -0500

    Removed files, creating new UI

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SignIn from "./components/LogIn";
-import SignUp from "./components/Register";
-import ForgotPassword from "./components/ForgotPassword";
 import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
-export default function App() {
-  return (
-    <Router>
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-
-      <Switch>
-        <Route path="/Register">
-          <Register />
-        </Route>
-
-        <Route path="/ForgotPassword">
-          <ResetPassword />
-        </Route>
-
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
-
-  function ResetPassword() {
-    return ForgotPassword();
-  }
-
-  function Home() {
-    return SignIn();
-  }
-
-  function Register() {
-    return SignUp();
-  }
-}
+export default function App() {}
